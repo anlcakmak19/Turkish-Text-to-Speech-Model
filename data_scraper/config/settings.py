2cff70aeaa59901f2ddbe0609042474b982c2561
kuzey253 <113684468+kuzey253@users.noreply.github.com>
2025-12-07 19:17:48 +0300

    Remove CONVERT_URL from settings

--- data_scraper/config/settings.py
+++ data_scraper/config/settings.py
@@ -4,6 +4,4 @@ class Config:
     MAX_LEN = 17.0
     PUNCT = (".", "?", "!")
     BASE_DIR = "./videos"
-
-    CONVERT_URL = "http://ovrargegpudev2:38009/convert/m4a-to-wav/"
     STT_URL = "https://api.elevenlabs.io/v1/speech-to-text"
